5a4987ca79c8ac716cc003445cdcfdba36202af2
Adam Leff <adamleff@gmail.com>
2016-01-25 01:10:45 -0800

    updated exercise7

--- class1/exercise7.py
+++ class1/exercise7.py
@@ -5,17 +5,16 @@
 import yaml, json
 from pprint import pprint
 
-if __name__ == "__main__":
 
 
-  with open("my_list.yml") as yaml_file:
+with open("my_list.yml") as yaml_file:
     yaml_file = yaml.load(yaml_file)
 
-  with open("my_list.json") as json_file:
+with open("my_list.json") as json_file:
     json_file = json.load(json_file)
 
 
 
-  pprint(yaml_file)
-  print "\n" + "\n"
-  pprint(json_file)
+pprint(yaml_file)
+print "\n" + "\n"
+pprint(json_file)

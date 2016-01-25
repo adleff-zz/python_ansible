4b736636f83c5de4d439eada81aad9e574a111da
Adam Leff <adamleff@gmail.com>
2016-01-25 01:10:33 -0800

    updated exercise6

--- class1/exercise6.py
+++ class1/exercise6.py
@@ -5,22 +5,21 @@
 
 import yaml, json
 
-if __name__ == "__main__":
 
-  my_list = range(10)
+my_list = range(10)
 
-  my_list.append('item1')
-  my_list.append('item2')
-  my_list.append({})
+my_list.append('item1')
+my_list.append('item2')
+my_list.append({})
 
-  my_list[-1]['model']  = 'Cisco 2951 ISR G2'
-  my_list[-1]['mgmt_ip'] = '10.1.10.1'
-  my_list[-1]['device_type'] = 'Router' 
-
+my_list[-1]['model']  = 'Cisco 2951 ISR G2'
+my_list[-1]['mgmt_ip'] = '10.1.10.1'
+my_list[-1]['device_type'] = 'Router' 
 
 
-  with open("my_list.yml", "w") as yaml_file:
+
+with open("my_list.yml", "w") as yaml_file:
     yaml_file.write(yaml.dump(my_list, default_flow_style=False))
 
-  with open("my_list.json", "w") as json_file:
+with open("my_list.json", "w") as json_file:
     json_file.write(json.dumps(my_list, json_file)) 
